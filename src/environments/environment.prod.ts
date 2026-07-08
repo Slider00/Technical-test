29d0b288c78d95b861941fa4c3098f1a1618a8d6
Julian <julian.correa.556@unisabaneta.edu.co>
2026-07-08 15:27:30 -0500

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: "AIzaSyDpI4u4w914w-DU1U7AQEJzWD6WfvgzZJs",
+    authDomain: "technical-test-3fe0a.firebaseapp.com",
+    projectId: "technical-test-3fe0a",
+    storageBucket: "technical-test-3fe0a.firebasestorage.app",
+    messagingSenderId: "373307689828",
+    appId: "1:373307689828:web:ac088ff573e08fddce8871",
+    measurementId: "G-E094TS3WB7"
   }
 };
