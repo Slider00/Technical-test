a324d9d65c992ff26768510c6e3f606f2881dbd2
Julian <julian.correa.556@unisabaneta.edu.co>
2026-07-08 16:14:11 -0500

    bug: styles

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.julian.technicaltest',
   appName: 'Technical-test',
   webDir: 'www'
 };
